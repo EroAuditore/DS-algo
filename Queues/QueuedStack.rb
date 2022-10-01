17636bf46b6ef63c84dab8577368ee00de5df10e
Eduardo <alfa.fenrir@gmail.com>
2022-10-01 12:13:08 -0500

    WIP Queues using stacks

--- Queues/QueuedStack.rb
+++ Queues/QueuedStack.rb
@@ -1,5 +1,11 @@
-require 'Stacks/stack'
+require_relative '../Stacks/stack'
 
 class QueuedStack
-    
+    attr_accessor :node, :value
+    def initialize(value)
+      @stackPush = Stack.new(value)
+      @value = value
+    end
 end
+
+qs = QueuedStack.new(1)
